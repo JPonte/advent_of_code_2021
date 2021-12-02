41285303e73723ef0f55e6dfdf8f4a2a32d7acfd
Joao Ponte <jpe@plista.com>
2021-12-02 18:10:49 +0100

    Day 2

--- src/day_02/main.rs
+++ src/day_02/main.rs
@@ -1,5 +1,45 @@
+#![feature(mixed_integer_ops)]
+
 use utils::*;
 
 fn main() {
-   let file = read_file("inputs/day_02/input.txt");
+    let file = read_file("inputs/day_02/input.txt");
+
+    let commands: Vec<(&str, u32)> = file
+        .iter()
+        .map(|l| {
+            let split: Vec<&str> = l.split(" ").collect();
+            (split[0], split[1].parse().unwrap())
+        })
+        .collect();
+
+    let mut horizontal_pos: u32 = 0;
+    let mut depth: u32 = 0;
+
+    for (command, amount) in commands.iter() {
+        match *command {
+            "forward" => horizontal_pos += *amount,
+            "up" => depth = depth.saturating_sub(*amount),
+            "down" => depth += *amount,
+            _ => {}
+        }
+    }
+    println!("First: {}", horizontal_pos * depth);
+
+    let mut aim: i32 = 0;
+    horizontal_pos = 0;
+    depth = 0;
+
+    for (command, amount) in commands.iter() {
+        match *command {
+            "forward" => {
+                horizontal_pos += *amount;
+                depth = depth.saturating_add_signed(aim * (*amount as i32));
+            }
+            "up" => aim -= *amount as i32,
+            "down" => aim += *amount as i32,
+            _ => {}
+        }
+    }
+    println!("Second: {}", horizontal_pos * depth);
 }
